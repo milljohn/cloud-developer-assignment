0d5ede569181d2e511cd04f65715f200f7e97e73
John Miller <jmiller@ecg-hq.com>
2021-05-04 21:06:22 -0500

    Update request method

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -5,11 +5,13 @@
 
 import './App.css';
 import { io } from "socket.io-client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-const URL = "http://localhost:5000/upload";
+const axios = require('axios')
+
+const url = "http://localhost:5000/upload";
 // import React from 'react'
-const socket = io(URL, { secure: false, reconnection: true, rejectUnauthorized: false });
+const socket = io(url, { secure: false, reconnection: true, rejectUnauthorized: false });
 
 function App() {
   useEffect(() => {
@@ -30,17 +32,53 @@ function App() {
     });
   }, []);
   // State to store uploaded file
-  const [file, setFile] = React.useState("");
+  const [file, setFile] = useState("");
 
   // Handles file upload event and updates state
   function handleUpload(event) {
-    setFile(event.target.files[0]);
-    console.log("File selected...")
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    console.log("File selected...");
+    console.log('selectedFile:', selectedFile);
+      console.log('path:', selectedFile.name)
+
+    // socket.emit("message", "file selected...")
 
 
     // Add code here to upload file to server
-    socket.emit("file", {"file" : event.target.files[0]})
+    // socket.emit("file", {"file" : event.target.files[0]})
     // This is not working correctly
+      const request_config = {
+          method: 'post',
+          url: 'http://localhost:5000/upload',
+          headers: {
+              'Content-Type': 'multipart/form-dataitem'
+          },
+          data: {
+              file: selectedFile
+          }
+      }
+      // todo: verify CORS is the problem
+      axios(request_config).then(res => {
+          console.log(`statusCode: ${res.statusCode}`)
+          console.log(res)
+      }).catch(error => {
+          console.log(error)
+      })
+
+      // axios.post(url,
+      //     selectedFile,
+      //     {
+      //       headers: {
+      //         'Content-Type': 'multipart/form-dataitem'
+      //       }
+      //     }).then(res => {
+      //     console.log(`statusCode: ${res.statusCode}`)
+      //     console.log(res)
+      // }).catch(error => {
+      //     console.log(error)
+      // })
+
   }
   return (
     <div className="App">
@@ -55,6 +93,7 @@ function App() {
           {file && <ImageThumb image={file} />}
 
           <p>Server Side:</p>
+          {/*TODO: read values from stuff  */}
           {/*{}*/}
     </div>
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
