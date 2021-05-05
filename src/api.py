88df40f9807f33f998f3d473580721ed6060459f
John Miller <jmiller@ecg-hq.com>
2021-05-04 20:16:23 -0500

    add CORS to api

--- src/api.py
+++ src/api.py
@@ -1,10 +1,15 @@
 import json
+import os.path
 import time
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
+from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
 
 # Flask object
 app = Flask(__name__)
+CORS(app)
+
 # app.register_blueprint(function_name, url_prefix="")
 # Starts the debugger
 app.config["DEBUG"] = True
@@ -12,16 +17,38 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-# @app.route('/upload', methods=["post"])
-@socketio.on('/', namespace='/upload')
-def upload(file):
-    print(json.dumps(file))
-    file_info = request.json['file']
-    metadata = [{""}]
-    emit(json.dumps(file), json=True, namespace="/flask-io-data", broadcast=True)
-    print(file_info)
-    return jsonify(metadata)
 
+@app.route('/upload', methods=['post'])
+@cross_origin()
+def upload_file():
+    try:
+        file = request.files['file']
+        file_path = secure_filename(file.filename)
+        file.save(file_path)
+        file_size = os.path.getsize(file_path)
+
+        response = {
+            'file_name': file_path,
+            'file_size': file_size
+        }
+
+        print(response)
+
+        emit(json.dumps(response), json=True, namespace='/upload', broadcast=True)
+
+        return response
+    except Exception as e:
+        print(e)
+        return {'error': str(e)}
+
+
+@app.route('/test', methods=['post'])
+@cross_origin()
+def test():
+    print(request.json['file_path'])
+    print(request)
+    emit({'received': True}, json=True, namespace='/test', broadcast=True)
+    return {'received': True}
 
 if __name__ == "__main__":
     # app.run()
